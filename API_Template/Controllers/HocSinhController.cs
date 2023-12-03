907eaad61a0409477cb68728652db6a5d9b7ea28
Windows <vtnduc2382000@gmail.com>
2023-12-03 10:23:37 +0700

    Add project files.

---
+++ API_Template/Controllers/HocSinhController.cs
@@ -0,0 +1,54 @@
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Template.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HocSinhController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<HocSinhController> _logger;
+
+        public HocSinhController(ILogger<HocSinhController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<HocSinhDetail> Get()
+        {
+            return new List<HocSinhDetail> {
+                new HocSinhDetail { Id=1,Name="Vương Vũ Tiệp"},
+                new HocSinhDetail { Id=2,Name="Maragret Wang"},
+                new HocSinhDetail { Id=3,Name="卢英雄"},
+                new HocSinhDetail {Id=4,Name="Võ Tấn Đức"}
+            };
+        }
+        [HttpGet("{id}")]
+        public HocSinhDetail Get(int id)
+        {
+            if(id==1)
+            {
+                return new HocSinhDetail() { Id = 1, Name = "Vương Vũ Tiệp" };
+
+            }
+            else if(id==2)
+            {
+                return new HocSinhDetail() { Id = 2, Name = "Maragret Wang" };
+            }
+            else if(id==3)
+            {
+                return new HocSinhDetail() { Id = 3, Name = "卢英雄" };
+            }
+            else
+            {
+                return new HocSinhDetail() { Id = 4, Name = "Võ Tấn Đức" };
+            }
+        }
+    }
+}
---
+++ Template/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using Template.Models;
+
+namespace Template.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        async public Task<IActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+            var data = await client.GetAsync("https://localhost:7299/HocSinh");
+            var res = await data.Content.ReadAsStringAsync();
+            var dataJson = JsonConvert.DeserializeObject<IEnumerable<HocSinhDetail>>(res);
+            return View(dataJson);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
